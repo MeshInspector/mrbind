0a096aea3843eb4ebb08b6762bc83767ef07d64a
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2024-07-09 11:09:48 -0500

    Uncomment headers.

--- include/mrbind/targets/pybind11.h
+++ include/mrbind/targets/pybind11.h
@@ -4,10 +4,10 @@
 
 #include <mrbind/targets/pybind11/bind_stl.h>
 
-// #if __has_include(<parallel_hashmap/phmap.h>)
-// #include <mrbind/targets/pybind11/bind_phmap.h>
-// #endif
+#if __has_include(<parallel_hashmap/phmap.h>)
+#include <mrbind/targets/pybind11/bind_phmap.h>
+#endif
 
-// #if __has_include(<tl/expected.hpp>)
-// #include <mrbind/targets/pybind11/bind_tl_expected.h>
-// #endif
+#if __has_include(<tl/expected.hpp>)
+#include <mrbind/targets/pybind11/bind_tl_expected.h>
+#endif
