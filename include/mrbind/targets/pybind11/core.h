853eee0f5c81d816eaf137b9c4d3a9c975cd3d2f
HolyBlackCat <holyblckcat@gmail.com>
2025-08-05 18:41:31 +0500

    Tweak comments.

--- include/mrbind/targets/pybind11/core.h
+++ include/mrbind/targets/pybind11/core.h
@@ -1663,7 +1663,7 @@ namespace MRBind::pb11
                     {
                         return pybind11::make_iterator<pybind11::return_value_policy::copy>(begin(target), end(target));
                     },
-                    // Looks like without `keep_alive` copied iterator might spoil out 
+                    // This one IS important, because the policy isn't `reference_internal`. Omitting this causes dangling in some cases, when the container is a temporary.
                     pybind11::keep_alive<0, 1>()
                 );
             }
