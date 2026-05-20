1025896f04df8e99015258d518d9d340b2d6dbca
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-05-20 06:53:21 -0500

    Revert accidental change to `example.h`.

--- examples/input/example.h
+++ examples/input/example.h
@@ -4,30 +4,13 @@
 
 namespace Example
 {
-    template <typename T>
-    struct Id
+    inline void SayHello()
     {
-        using ValueType = T;
+        std::cout << "Hello!\n";
+    }
 
-        template <typename U = int
-    // The condition is there for the binding generator. Firstly, removing the second template argument produces better function names in C,
-        //   and secondly `--buggy-substitute-default-template-args` seems to duplicate this constructor when `enable_if_t` is present.
-        #if MR_HAS_REQUIRES
-        > requires std::is_integral_v<U>
-        #else
-        , std::enable_if_t<std::is_integral_v<U>, std::nullptr_t> = nullptr>
-        #endif
-        explicit constexpr Id( U i ) noexcept : id_( ValueType( i ) ) { }
-
-        Id() {}
-        T id_;
-    };
-
-    template <>
-    struct Id<int> {};
-
-    inline void use()
+    inline int Square(int x)
     {
-        Id<int> id;
+        return x * x;
     }
 }
