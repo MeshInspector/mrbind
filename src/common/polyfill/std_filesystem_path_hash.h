89f2de093fa29d830efdc2c17433f6449fcb0d97
Egor Mikhaylov <egor.mikhaylov@meshinspector.com>
2026-06-15 05:37:14 -0500

    Fix build on libstdc++ 11.4+ as suggested in #36.

--- src/common/polyfill/std_filesystem_path_hash.h
+++ src/common/polyfill/std_filesystem_path_hash.h
@@ -2,9 +2,9 @@
 
 #include <version>
 
-// Old libstdc++ didn't have `std::hash<std::filesystem::path>`, which was apparently added in a defect report.
-// GCC added it in this commit: https://github.com/gcc-mirror/gcc/commit/e3c5e8360b4e4799e1e2daf74282629248690f23
-#if defined(_GLIBCXX_RELEASE) && _GLIBCXX_RELEASE < 12
+// Old libstdc++ didn't have `std::hash<std::filesystem::path>`, which was added in a defect report: https://cplusplus.github.io/LWG/issue3657
+// It exists since libstdc++ 11.4 (first added to trunk in https://github.com/gcc-mirror/gcc/commit/e3c5e8360b4e4799e1e2daf74282629248690f23, then backported to 11.x).
+#if defined(_GLIBCXX_RELEASE) && (_GLIBCXX_RELEASE < 11 || (_GLIBCXX_RELEASE == 11 && __GLIBCXX__ < 20230528))
 
 #include <cstddef>
 #include <filesystem>
